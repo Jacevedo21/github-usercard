b09bfa2285e3e5416ffc1bca3f178c5976b3d40c
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-09 18:41:43 -0400

    finished

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,14 @@
     https://api.github.com/users/<your name>
 */
 
+import axios from 'axios'
+
+// const gitHubURL = 'https://api.github.com/users/Jacevedo21'
+
+
+
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -17,6 +25,7 @@
     and append the returned markup to the DOM as a child of .cards
 */
 
+
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,7 +37,7 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +58,84 @@ const followersArray = [];
       </div>
     </div>
 */
+function layout(object) {
+  const card = document.createElement('div')
+  const img = document.createElement('img')
+  const info = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const link = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+ 
+  card.className = 'card'
+  name.className = 'name'
+  info.className = 'card-info'
+  username.className = 'username'
+
+  link.href = object.html_url
+  
+  name.textContent = object.name
+  username.textContent = object.username
+  location.textContent= object.location
+  profile.textContent = 'Profile: '
+  link.textContent = object.html_url
+  followers.textContent = `Followers: ${object.followers}`
+  followers.textContent = `Following: ${object.following}`
+  bio.textContent = `Bio: ${object.bio}`
+  img.src = object.avatar_url
+
+  card.appendChild(img)
+  card.appendChild(info)
+  info.appendChild(name)
+  info.appendChild(username)
+  info.appendChild(location)
+  info.appendChild(profile)
+  profile.appendChild(link)
+  info.appendChild(followers)
+  info.appendChild(following)
+  info.appendChild(bio)
+
+
+  return card
+  
+}
+
+console.log()
+
+
+const followersArray = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell',
+  'rofstudios',
+  'jlosito',
+  'hernandezM-dev',
+  'yirano',
+  'davichodavid',
+];
+
+followersArray.forEach( element => {
+  const userGit = `https://api.github.com/users/${element}`
+  const cards = document.querySelector('.cards')
+  
+  axios.get(userGit)
+  .then(function (response) {
+    // console.log(response.data)
+    // let info = something.data
+    let style = layout(response.data)
+    cards.appendChild(style)
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+})
+
 
 /*
   List of LS Instructors Github username's:
